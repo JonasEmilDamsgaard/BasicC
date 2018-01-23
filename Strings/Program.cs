77a2b2e39518eff11c0c3942af6ea81dc59bb39b
Jonas <admin@TA-8211-07>
2018-01-23 14:35:05 +0100

    New project (Table)

---
+++ Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Mikkel";
+            string lastName = "Cronberg";
+            string fullName = firstName + " " +lastName;
+            Console.WriteLine(fullName);
+            Console.WriteLine(fullName.ToLower());
+            Console.WriteLine(fullName.ToUpper());
+            Console.WriteLine(fullName.Substring(7, 4));
+            string[] nameArray = fullName.Split(' ');
+
+            foreach (string name in nameArray)
+            {
+                Console.WriteLine(name);
+            }
+
+            System.IO.File.WriteAllText(@"C:\temp\temp.txt", fullName);
+
+            Console.ReadLine();
+            
+
+        }
+    }
+}
---
+++ Table/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nextNumber = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    nextNumber = j * i;
+
+                    if (nextNumber >= 50)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    Console.Write(nextNumber.ToString().PadLeft(5));
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

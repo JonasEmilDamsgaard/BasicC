79976e99ffc98f7cea1ee642b6a67a3853ddccf1
JonasEmilDamsgaard <jonasdamsgaard@gmail.com>
2018-02-05 11:29:31 +0100

    Yahtzee

--- Module06_IntroOOP/Program.cs
+++ Module06_IntroOOP/Program.cs
@@ -10,6 +10,9 @@ namespace Module06_IntroOOP
     {
         static void Main(string[] args)
         {
+            var p1 = new Person() { firstName = "Jonas", lastName = "Damsgaard", birthDate = 1984};
+            Console.WriteLine(p1.Fullname());
+            Console.WriteLine(p1.Age().ToString());
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
@@ -18,4 +21,35 @@ namespace Module06_IntroOOP
             }
         }
     }
+
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int birthDate;
+
+        public Person()
+        {
+            firstName = "";
+            lastName = "";
+        }
+
+        public Person(string firstName, string lastName, int birthDate)
+        {
+            this.firstName = firstName.ToUpper();
+            this.lastName = lastName.ToUpper();
+            this.birthDate = birthDate;
+        }
+
+        public string Fullname()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public int Age()
+        {
+            return DateTime.Now.Year - birthDate;
+        }
+    }
+
 }
---
+++ Yahtzee/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+    public class Dice
+    {
+        private int Eyes;
+
+
+    }
+
+    public class Cup
+    {
+
+    }
+}
